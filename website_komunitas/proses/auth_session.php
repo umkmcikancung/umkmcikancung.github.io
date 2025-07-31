a0a019151afc0dac3ee62d88cb993b8966889aea
af1410 <107295079+af1410@users.noreply.github.com>
2025-07-31 20:46:40 +0700

    Delete website_komunitas directory

--- website_komunitas/proses/auth_session.php
+++
@@ -1,1 +0,0 @@
-<?php session_start(); if(!isset($_SESSION['login'])){ header('Location: ../pages/login.php'); exit(); } ?>
--- website_komunitas/proses/edit_berita_proses.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../config/koneksi.php';
-$id = $_POST['id'];
-$judul = $_POST['judul'];
-$isi = $_POST['isi'];
-mysqli_query($conn, "UPDATE berita SET judul='$judul', isi='$isi' WHERE id=$id");
-header("Location: ../pages/admin/admin.php");
